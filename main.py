800eafabed9f4a8f5cffa7270cf4ad6266a5f2a4
fitandfine <emailofanup@gmail.com>
2026-01-17 22:36:46 +1300

    Add /upload endpoint and update requirements for file handling. Haven't tested the endpoint
    - Prototyped a new POST /upload endpoint to handle log file uploads.
    - Updated requirements.txt to include python-multipart for file handling.
    -Learned about "tage" parameter for routes. Tags are used for grouping endpoints in the documentation used by Swagger UI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #This file contains the entry point to the entire application.
+from typing import Dict, Any, List
 from fastapi import FastAPI, HTTPException, UploadFile, File
 
 app = FastAPI(
@@ -6,8 +7,8 @@ app = FastAPI(
     description=" An API to upload and parse log files to extract errors and warnings",
     version="0.1.0"
 )
-@app.get("/")
-def health_check():
+@app.get("/", tags=["Health Check"]) # tags is used for grouping endpoints in the documentation used by Swagger UI
+def health_check() -> Dict[str, str]:
     """
     root path to check if the API is running.
     Purpose:
@@ -20,3 +21,8 @@ def health_check():
         "status": "online",
         "message": "Log Analyzer API is running."
         }
+
+
+@app.post("/upload", tags=["Log Upload"])
+async def upload_log(file : UploadFile = File(...)) -> Dict[str,Any]:
+    return {"filename": file.filename, "content_type": file.content_type}
